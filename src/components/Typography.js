d9dc4c0ef00fbed71e0339fec25c698f47d92be0
nctins <nguyencongtin2000@gmail.com>
2022-07-13 23:15:50 +0700

    Update Typo component

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Text } from "react-native";
 import useTheme from "~hooks/useTheme";
 
-const Typography = (props) => {
+const Typography = ({ variant = "Text", style, children, ...otherProps }) => {
   const theme = useTheme();
   const typography = theme.typography;
-  const { variant, style, ...otherProps } = props;
-  const textStyle = variant ? typography[variant] : typography.Text;
   return (
-    <Text style={[textStyle, style]} {...otherProps}> {props.children} </Text>
+    <Text style={[typography[variant], style]} {...otherProps}>
+      {children}
+    </Text>
   );
 };
 
