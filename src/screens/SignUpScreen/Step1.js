c3b1d6ee5f7d7c4ef7c6d1cf765114e785297240
nctins <nguyencongtin2000@gmail.com>
2022-07-15 21:22:44 +0700

    update signUp step 1 screen

--- src/screens/SignUpScreen/Step1.js
+++ src/screens/SignUpScreen/Step1.js
@@ -4,12 +4,37 @@ import * as Icons from "~components/Icons";
 import { SIGNUP_BG } from "assets/images";
 import { View } from "react-native";
 import { TextInput } from "~components/Inputs";
+import Button, { IconButton } from "~components/Button";
+import { BackIcon } from "~components/Icons";
+import Typography from "~components/Typography";
 
 const Step1 = () => {
   return (
     <BgImageLayout background={SIGNUP_BG}>
-      <View style={{ flex: 1, alignItems:'center'}}>
-        <TextInput placeholder="hello touch here to edit" isPassword title={"Password"}/>
+      <View style={{alignItems:"flex-start"}}>
+        <IconButton style={{margin: 20}} icon={<BackIcon color="Gray.0" />} />
+      </View>
+      <View style={{alignItems:"center", marginVertical:20}}>
+        <Typography variant="H5" color="Gray.0" >Đăng ký</Typography>
+      </View>
+      <View style={{alignItems: "center" }}>
+        <TextInput
+          placeholder="Hứa không gửi email spam"
+          title={"Email"}
+        />
+        <TextInput
+          placeholder="Mọi người liên lạc bạn theo số này nè"
+          title={"Số điện thoại"}
+        />
+        <TextInput
+          placeholder="Bạn thích mọi người gọi bạn là gì ?"
+          title={"Tên"}
+        />
+      </View>
+      <View style={{alignItems:"center"}}>
+        <Button size="lg">
+          Tiếp theo
+        </Button>
       </View>
     </BgImageLayout>
   );
