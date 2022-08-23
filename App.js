6574f49375ec9bf483ad9f3d5421479e2c23b7b9
vuprodtkt <vuprodtkt1111@gmail.com>
2022-08-23 23:42:09 +0700

    add voucher detail screen

--- App.js
+++ App.js
@@ -6,12 +6,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from '~contexts/ThemeContext';
 import MessageDetail from '~screens/Message/MessageDetail';
 import HomeScreen from '~screens/HomeScreen';
+import CouponDetail from '~screens/CouponDetail';
 
 export default function App() {
   return (
     <ThemeProvider>
       <NavigationContainer>
-        <RootNavigator />
+        <CouponDetail />
       </NavigationContainer>
     </ThemeProvider>
   );
