891328fce25779737e94860af9f18fbcd83a342f
nctins <nguyencongtin2000@gmail.com>
2022-08-24 00:34:42 +0700

    add login screen

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import RootNavigator from '~navigation/RootNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from '~contexts/ThemeContext';
-import StartScreen from '~screens/StartScreen';
+import LoginScreen from '~screens/LoginScreen';
 
 export default function App() {
   return (
     <ThemeProvider>
       {/* <NavigationContainer> */}
         {/* <RootNavigator /> */}
-        <StartScreen/>
+        <LoginScreen/>
       {/* </NavigationContainer> */}
     </ThemeProvider>
   );
