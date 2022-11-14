948ebe42bf65446a9e2f0d282acd08d5caaa6e1c
nctins <nguyencongtin2000@gmail.com>
2022-11-14 23:28:10 +0700

    add socket context

--- App.js
+++ App.js
@@ -4,15 +4,18 @@ import { ThemeProvider } from '~contexts/ThemeContext';
 import InitNavigator from '~navigation/InitNavigator';
 import { AuthProvider } from '~contexts/AuthContext';
 import { AxiosProvider } from '~contexts/AxiosContext';
+import { SocketProvider } from '~contexts/SocketContext';
 
 export default function App() {
   return (
     <AuthProvider>
       <ThemeProvider>
         <AxiosProvider>
-          <NavigationContainer>
-            <InitNavigator />
-          </NavigationContainer>
+          <SocketProvider>
+            <NavigationContainer>
+              <InitNavigator />
+            </NavigationContainer>
+          </SocketProvider>
         </AxiosProvider>
       </ThemeProvider>
     </AuthProvider>
