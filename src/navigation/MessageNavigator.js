3aaf3c61ec74749d77fd47848fddad8d839370e1
nctins <nguyencongtin2000@gmail.com>
2022-11-14 23:31:02 +0700

    add chat feature

--- src/navigation/MessageNavigator.js
+++ src/navigation/MessageNavigator.js
@@ -1,15 +1,23 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MessageScreen from '~screens/Message/MessageScreen';
-import MessageDetail from '~screens/Message/MessageDetail';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MessageScreen from "~screens/Message/MessageScreen";
+import MessageDetail from "~screens/Message/MessageDetail";
 
 const MessageStack = createNativeStackNavigator();
 
 const MessageNavigator = () => {
-    return (
-        <MessageStack.Navigator initialRouteName="MessageScreen" >
-            <MessageStack.Screen name="MessageScreen" component={MessageScreen} options={{ headerShown: false }} />
-            <MessageStack.Screen name="MessageDetail" component={MessageDetail} options={{ headerShown: false }} />
-        </MessageStack.Navigator>
-    );
-}
-export default MessageNavigator
+  return (
+    <MessageStack.Navigator initialRouteName="MessageScreen">
+      <MessageStack.Screen
+        name="MessageScreen"
+        component={MessageScreen}
+        options={{ headerShown: false }}
+      />
+      <MessageStack.Screen
+        name="MessageDetail"
+        component={MessageDetail}
+        options={{ headerShown: false }}
+      />
+    </MessageStack.Navigator>
+  );
+};
+export default MessageNavigator;
--- src/screens/Message/MesageTab.js
+++ src/screens/Message/MesageTab.js
@@ -1,66 +1,69 @@
-import React, {useContext} from 'react'
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import { StyleSheet, View, ScrollView, TextInput } from "react-native";
-import MessageItem from '~components/MessageItem';
-import useThemeStyles from '~hooks/useThemeStyles';
-import { AxiosContext } from '~contexts/AxiosContext';
+import MessageItem from "~components/MessageItem";
+import { AxiosContext } from "~contexts/AxiosContext";
+import useThemeStyles from "~hooks/useThemeStyles";
 
-const styles = (theme) => StyleSheet.create({
-  default: {
-    flex: 1,
-    backgroundColor: "white",
-    paddingHorizontal: 15,
-    flexDirection: "column",
-    justifyContent: "center"
-  },
-  content:{
-    flexGrow:1,
-    flexDirection: "column",
-    alignItems: "center",
-    backgroundColor: "white"
-  },
-  TextInputView: {
-    width: "100%",
-    flexDirection:"row",
-    justifyContent: "center"
-  },
-  textInput: {
-    width: "90%",
-    height: 40,
-    borderRadius: 15,
-    paddingHorizontal: 15,
-    paddingVertical: 5,
-    backgroundColor: theme.colors.Gray[1],
-    marginTop: 10,
-    marginBottom: 10,
-  },
-})
+const styles = (theme) =>
+  StyleSheet.create({
+    default: {
+      flex: 1,
+      backgroundColor: theme.colors.Gray[0],
+      paddingHorizontal: 15,
+      flexDirection: "column",
+      justifyContent: "center",
+    },
+    content: {
+      flexGrow: 1,
+      flexDirection: "column",
+      alignItems: "center",
+      backgroundColor: theme.colors.Gray[0],
+    },
+    TextInputView: {
+      width: "100%",
+      flexDirection: "row",
+      justifyContent: "center",
+    },
+    textInput: {
+      width: "90%",
+      height: 40,
+      borderRadius: 15,
+      paddingHorizontal: 15,
+      paddingVertical: 5,
+      backgroundColor: theme.colors.Gray[1],
+      marginTop: 10,
+      marginBottom: 10,
+    },
+  });
 
-const MessageTab = ({navigation}) => {
+const MessageTab = ({ navigation }) => {
   const style = useThemeStyles(styles);
-  const {authAxios} = useContext(AxiosContext);
-
-  const box_chats = authAxios.get("/box-chats").then((res)=>{
-    console.log(res.data);
-    return res.data;
-  }).catch((err)=>{
-    console.log(err.response.data);
-    return [];
-  })
-
+  const { authAxios } = useContext(AxiosContext);
+  const [boxChats, setBoxChats] = useState([]);
+  useEffect(()=>{
+    authAxios
+    .get("/box-chats")
+    .then((res) => {
+      const resObj = res.data;
+      setBoxChats(resObj.data);
+    })
+    .catch((err) => {
+      console.log(err.response.data);
+      setBoxChats([]);
+    });
+  },[]);
 
   return (
     <View style={style.default}>
       <View style={style.TextInputView}>
-        <TextInput 
-          style={style.textInput}
-          placeholder="Search"
-        />
+        <TextInput style={style.textInput} placeholder="Search" />
       </View>
-      <ScrollView  contentContainerStyle={style.content}>
-        <MessageItem navigation={navigation} />
-        <MessageItem navigation={navigation} />
+      <ScrollView contentContainerStyle={style.content}>
+        {boxChats.map((box_chat, idx) => (
+          <MessageItem key={idx} navigation={navigation} data={box_chat} />
+        ))}
       </ScrollView>
-    </View >
-  )
-}
-export default MessageTab
+    </View>
+  );
+};
+export default MessageTab;
