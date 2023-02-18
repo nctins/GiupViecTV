76c5e092d06b6ae07e0fc92c50998018898ed0e5
vuprodtkt <vuprodtkt1111@gmail.com>
2023-02-18 09:29:34 +0700

    add location

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default function App() {
         <AxiosProvider>
           <SocketProvider>
             <NavigationContainer>
-              <InitNavigator />
+              <GoogleMap />
             </NavigationContainer>
           </SocketProvider>
         </AxiosProvider>
--- src/screens/GoogleMapScreen.js
+++ src/screens/GoogleMapScreen.js
@@ -2,21 +2,23 @@ import React, { useState, useEffect, useContext, useRef } from 'react';
 import { StyleSheet, View, Dimensions, SafeAreaView,StatusBar,ScrollView,TextInput,RefreshControl } from "react-native";
 import useThemeStyles from '~hooks/useThemeStyles';
 import Typography from "~components/Typography";
+import Button from '~components/Button';
 import { AuthContext } from "~contexts/AuthContext";
 import { AxiosContext } from "~contexts/AxiosContext";
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import * as Location from 'expo-location';
 import { API_GOOGLE } from "../constants/api";
 
 const { width, height } = Dimensions.get("window");
 
-const SearchAddressComponent = ({onPlaceSelected}) => {
+const SearchAddressComponent = ({onPlaceSelected, address}) => {
     const style = useThemeStyles(styles);
 
     return (
         <GooglePlacesAutocomplete
             styles={{textInput: style.input}}
-            placeholder='Search'
+            placeholder= {address}
             fetchDetails={true}
             onPress={(data, details) => {
                 // 'details' is provided when fetchDetails = true
@@ -44,7 +46,36 @@ const GoogleMap = ({}) => {
                                             longitude: 106.6752672,
                                             latitudeDelta: 0.01,
                                             longitudeDelta: cal_longitude_Delta(0.01)});
+    const [address, setAddress] = useState("");
     const mapRef = useRef(null);
+
+    useEffect(() => {
+        (async () => {
+          
+          let { status } = await Location.requestForegroundPermissionsAsync();
+          if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+          }
+    
+          let location = await Location.getCurrentPositionAsync({});
+        //   console.log(location);
+        //   setPosition({...position, latitude: location.coords.latitude, longitude: location.coords.longitude});
+        reverseGeocode();
+        })();
+    }, []);
+
+    const getAddress = (address) => {
+        return address.streetNumber + " " + address.street + " " + address.subregion + " " + address.region;
+    }
+
+    const reverseGeocode = async () => {
+        const reverseGeocodeAddress = await Location.reverseGeocodeAsync({
+            latitude: position.latitude,
+            longitude: position.longitude,
+        });
+        setAddress(getAddress(reverseGeocodeAddress[0]));
+    }
 
     const moveTo = async () => {
         const camera = await mapRef.current?.getCamera();
@@ -59,20 +90,32 @@ const GoogleMap = ({}) => {
         }
     };
 
+    const zoomIn = () => {
+        const { latitudeDelta, longitudeDelta } = position;
+        setPosition({...position,latitudeDelta: latitudeDelta * 0.5,longitudeDelta: longitudeDelta * 0.5});
+    };
+    
+    const zoomOut = () => {
+        const { latitudeDelta, longitudeDelta } = position;
+        setPosition({...position,latitudeDelta: latitudeDelta * 2,longitudeDelta: longitudeDelta * 2});
+    };
+
     const onPlaceSelected = (details) => {
         // const position = {
         //   latitude: details?.geometry.location.lat || 0,
         //   longitude: details?.geometry.location.lng || 0,
         // };
         // setPosition(position);
+        console.log(details);
         setPosition(prev => {return {...prev,latitude: details.geometry.location.lat, longitude: details.geometry.location.lng}})
-        moveTo();
+        // moveTo();
     };
 
     const onPressMap = (pos) => {
         // console.log(position);
+        reverseGeocode();
         setPosition(prev => {return {...prev, latitude:pos.latitude, longitude:pos.longitude}});
-        moveTo();
+        // moveTo();
     }
 
     return (
@@ -82,14 +125,17 @@ const GoogleMap = ({}) => {
                 style={style.map} 
                 provider={PROVIDER_GOOGLE} 
                 ref={mapRef}
-                initialRegion={position}
+                region={position}
                 onPress={(e) => onPressMap(e.nativeEvent.coordinate)}
             >
                 {position && <Marker coordinate={{latitude: position.latitude, longitude: position.longitude}} />}
             </MapView>
             <View style={style.searchContainer}>
-                <SearchAddressComponent onPlaceSelected = {onPlaceSelected}/>
+                <SearchAddressComponent onPlaceSelected = {onPlaceSelected} address={address} />
             </View>
+            <Button style = {style.zoomInButton} radius={0} onPress={zoomIn}>+</Button>
+            <Button style = {style.zoomOutButton} radius={0} onPress={zoomOut}>-</Button>
+
         </View>
     );
 }
@@ -122,14 +168,33 @@ const styles = (theme) => StyleSheet.create({
         borderColor: "#888",
         borderWidth: 1,
     },
-    button: {
-        backgroundColor: "#bbb",
-        paddingVertical: 12,
-        marginTop: 16,
-        borderRadius: 4,
+    zoomInButtonContainer: {
+        position: "absolute",
+        width: 40,
+        height: 40,
+        backgroundColor: "white",
+        top: height - 180,
+        left: width - 70,
     },
-    buttonText: {
-        textAlign: "center",
+    zoomInButton:{
+        position: "absolute",
+        width:  40,
+        height: 40,
+        paddingHorizontal: 0,
+        paddingVertical: 0,
+        backgroundColor: theme.colors.BackgroundBlue,
+        top: height - 180,
+        left: width - 70,
+    },
+    zoomOutButton:{
+        position: "absolute",
+        width:  40,
+        height: 40,
+        paddingHorizontal: 0,
+        paddingVertical: 0,
+        backgroundColor: theme.colors.BackgroundBlue,
+        top: height - 130,
+        left: width - 70,
     },
   })
 
