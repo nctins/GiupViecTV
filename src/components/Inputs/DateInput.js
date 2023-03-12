4905be9b81d1d7de0b0fda5ae039764ce520b153
nctins <nguyencongtin2000@gmail.com>
2023-03-12 23:35:22 +0700

    update date time input component

--- src/components/Inputs/DateInput.js
+++ src/components/Inputs/DateInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Button, Text, TouchableOpacity, StyleSheet } from "react-native";
 import {
   DateTimePicker,
@@ -22,28 +22,55 @@ const styles = (theme) =>
     },
   });
 
-const DateInput = ({value, onChange}) => {
+const DateInput = ({ value = null, auto_completed = true, ...props }) => {
+  const current = new Date();
+  const [date, setDate] = useState(null);
+
+  useEffect(() => {
+    if (auto_completed) {
+      const init = value || current;
+      setDate(init);
+    } else {
+      setDate(value);
+    }
+  }, [value, date])
+
+  const onChange = (event, selectedDate) => {
+    selectedDate.setHours(0,0,0,0);
+    if(event.type === 'set') {
+      if (props.onChange) {
+        props.onChange(selectedDate);
+      }
+      setDate(selectedDate);
+    } else {
+      setDate(null);
+    }
+  };
 
   const showModel = () => {
     DateTimePickerAndroid.open({
-      value,
+      value: date || current,
       onChange,
       mode: "date",
       is24Hour: true,
+      minimumDate: current,
     });
   };
 
   const style = useThemeStyles(styles);
   return (
     <TouchableOpacity
-      style={style.input}
+      style={[style.input, props.containerStyle]}
       onPress={showModel}
       activeOpacity={1}
     >
       <Text>
-        {value.getDate() + "/" + (value.getMonth() + 1)+ "/" + value.getFullYear()}
+        {!date ?
+          ""
+          : `${date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()}`
+        }
       </Text>
-      <CalendarIcon size="sm"/>
+      <CalendarIcon size="sm" />
     </TouchableOpacity>
   );
 };
--- src/components/Inputs/TimeInput.js
+++ src/components/Inputs/TimeInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Button, Text, TouchableOpacity, StyleSheet } from "react-native";
 import {
   DateTimePicker,
@@ -22,26 +22,52 @@ const styles = (theme) =>
     },
   });
 
-const TimeInput = ({value, onChange}) => {
+const TimeInput = ({auto_completed=true, value=null, ...props}) => {
+  const current = new Date();
+  const [date, setDate] = useState(null);
+  useEffect(()=>{
+    if (auto_completed) {
+      const init = value || current;
+      setDate(init);
+    } else {
+      setDate(value);
+    }
+  }, [value, date])
+
+  const onChange = (event, selectedDate) => {
+    selectedDate.setSeconds(0);
+    selectedDate.setMilliseconds(0);
+    if (event.type === 'set') {
+      if ( props.onChange) {
+        props.onChange(selectedDate);
+      }
+      console.log(selectedDate.getMiliseconds());
+      console.log(selectedDate.toTimeString());
+      setDate(selectedDate);
+    } else {
+      setDate(null)
+    }
+  };
 
   const showModel = () => {
     DateTimePickerAndroid.open({
-      value,
+      value: date || current,
       onChange,
       mode: "time",
       is24Hour: true,
     });
   };
 
+
   const style = useThemeStyles(styles);
   return (
     <TouchableOpacity
-      style={style.input}
+      style={[style.input,props.containerStyle]}
       onPress={showModel}
       activeOpacity={1}
     >
       <Text>
-        {value.toLocaleTimeString().slice(0,5)}
+        {!date ? "" : date.toLocaleTimeString().slice(0,5)}
       </Text>
       <ClockIcon size="sm"/>
     </TouchableOpacity>
